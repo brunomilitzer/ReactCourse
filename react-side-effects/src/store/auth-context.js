b01f11e958ee7e696752c58a5cc460fb6c225221
Bruno Militzer <brunomilitzer.work@gmail.com>
2021-04-28 22:40:15 +0100

    Refactored Input component

--- react-side-effects/src/store/auth-context.js
+++ react-side-effects/src/store/auth-context.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const AuthContext = React.createContext({
-    isLoggedIn: false
+    isLoggedIn: false,
+    onLogout: () => {},
+    onLogin: (email, password) => {}
 });
+
+export const AuthContextProvider = (props) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const loggedIn = localStorage.getItem('isLoggedIn');
+
+        if (loggedIn === '1') {
+            setIsLoggedIn(true);
+        }
+    }, []);
+
+    const logoutHandler = () => {
+        console.log('logout handler');
+        localStorage.removeItem('isLoggedIn');
+        setIsLoggedIn(false);
+    };
+
+    const loginHandler = () => {
+        console.log('login handler');
+        localStorage.setItem('isLoggedIn', '1');
+        setIsLoggedIn(true);
+    }
+
+   return <AuthContext.Provider
+       value={{isLoggedIn: isLoggedIn, onLogout: logoutHandler, onLogin: loginHandler}}>{props.children}</AuthContext.Provider>
+}
 
 export default AuthContext;
