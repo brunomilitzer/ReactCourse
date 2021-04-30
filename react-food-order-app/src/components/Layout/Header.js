a6e7e35d5d1b51262c78b9a42fc42e114fc63a82
brunomilitzer <bruno.militzer@est.tech>
2021-04-30 21:01:45 +0100

    Created Add Ammount in Cart

--- react-food-order-app/src/components/Layout/Header.js
+++ react-food-order-app/src/components/Layout/Header.js
@@ -1,19 +1,17 @@
-import {Fragment, useContext} from "react";
+import {Fragment} from "react";
 
 import mealsImage from '../../assets/meals.jpg';
 import HeaderCardButton from "./HeaderCardButton";
-import CartContext from "../../store/cart-context";
 
 import classes from './Header.module.css';
 
-const Header = () => {
-    const cartCtx = useContext(CartContext);
+const Header = (props) => {
 
     return (
         <Fragment>
             <header className={classes.header}>
                 <h1>ReactMeals</h1>
-                <HeaderCardButton onClick={cartCtx.onShowCart} />
+                <HeaderCardButton onClick={props.onShowCart} />
             </header>
             <div className={classes['main-image']}>
                 <img src={mealsImage} alt="A table full of delicious food!"/>
